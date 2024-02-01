39c34903575d2aa3b855a396066a23ad46e456de
harris <harris.kunakira@gmail.com>
2024-02-01 20:56:30 +0700

    projects configuration

--- svelte-poc/svelte.config.js
+++ svelte-poc/svelte.config.js
@@ -12,9 +12,10 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		// paths: {
-		// 	base: '/v1'
-		// },
+		paths: {
+			assets: 'http://localhost/v1', // important for locating assets if using path rewriting in the server
+			// base: '/v1' // always required to use {base}/path/to/assets
+		},
 	},
 };
 
